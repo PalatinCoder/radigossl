e8fe5834e725c7c9cbfbd7dac047f828ed65aa62
Jan Syring-Lingenfelder <github@jan-sl.de>
2020-06-10 22:15:09 +0200

    feat: exit the app

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module jan-sl.de/radiossl
 
 go 1.14
 
-require github.com/rivo/tview v0.0.0-20200528200248-fe953220389f
+require (
+	github.com/gdamore/tcell v1.3.0
+	github.com/rivo/tview v0.0.0-20200528200248-fe953220389f
+)
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
 
@@ -13,6 +14,12 @@ func main() {
 	title := tview.NewTextView()
 
 	title.SetText("\n\n[orange]sunshine live\n\n[blue]electronic music radio").SetTextAlign(tview.AlignCenter).SetDynamicColors(true)
+	title.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyESC {
+			app.Stop()
+		}
+		return event
+	})
 	if err := app.SetRoot(title, true).Run(); err != nil {
 		panic(err)
 	}
