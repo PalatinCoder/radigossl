73cda95920acc3976049e1868febe85cf9c29b7b
Jan Syring-Lingenfelder <github@jan-sl.de>
2020-06-13 14:47:50 +0200

    refactor(main): some comments and logging

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 const tag = "main"
 
 func main() {
+
+	// setup logging
 	file, err := os.OpenFile("radigossl.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
@@ -20,18 +22,22 @@ func main() {
 
 	log.Printf("[%s] Starting application", tag)
 
+	// setup ui
 	app := tview.NewApplication()
 	title := tview.NewTextView()
 
 	title.SetText("\n\n[orange]sunshine live\n\n[blue]electronic music radio").SetTextAlign(tview.AlignCenter).SetDynamicColors(true)
 	title.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() == tcell.KeyESC {
+			log.Printf("[%s/ui] exit", tag)
 			app.Stop()
 		}
 		return event
 	})
+	log.Printf("[%s/ui] starting ui", tag)
 	if err := app.SetRoot(title, true).Run(); err != nil {
-		panic(err)
+		log.Fatalf("[%s] ui initialization failed: %d", tag, err)
+	log.Printf("[%s] End", tag)
 	}
 
 	log.Print("Ending")
